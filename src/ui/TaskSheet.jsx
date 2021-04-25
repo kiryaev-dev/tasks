d3930421695f9f077ff7b59c6dec2d77b180ad74
kiryaev-dev <kiryaev.dev@gmail.com>
2021-04-25 22:12:46 +0500

    Added completed tasks list

--- src/ui/TaskSheet.jsx
+++ src/ui/TaskSheet.jsx
@@ -3,14 +3,16 @@ import "./TaskSheet.css"
 
 import Card from "@material-ui/core/Card"
 import {Divider, List, Typography} from "@material-ui/core"
-import {AddingTask, PendingTask} from "./Task"
+import {AddingTask, CompletedTask, PendingTask} from "./Task"
 
 export default function TaskSheet({model}) {
     const {title} = model
     const [pending, setPending] = useState(Array.from(model.pending))
+    const [completed, setCompleted] = useState(Array.from(model.completed))
 
     function updateState() {
         setPending(Array.from(model.pending))
+        setCompleted(Array.from(model.completed))
     }
 
     function complete(task) {
@@ -22,6 +24,18 @@ export default function TaskSheet({model}) {
         model.pending.add(task)
         updateState()
     }
+
+    const CompletedList = (
+        <>
+            <Divider />
+            <List>
+                {
+                    completed.map((task, index) =>
+                        <CompletedTask model={task} key={index} onComplete={_ => complete(task)} />)
+                }
+            </List>
+        </>
+    )
 
     return (
         <Card className="task-sheet" elevation={1}>
@@ -36,6 +50,7 @@ export default function TaskSheet({model}) {
                 }
                 <AddingTask onAdd={add} />
             </List>
+            {completed.length > 0 ? CompletedList : undefined}
         </Card>
     )
 }
