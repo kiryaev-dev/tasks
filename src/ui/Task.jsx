348a9229aaccb07713b24538ffe1fcfcd2429f3b
kiryaev-dev <kiryaev.dev@gmail.com>
2021-04-25 17:52:24 +0500

    Added onComplete callback

--- src/ui/Task.jsx
+++ src/ui/Task.jsx
@@ -4,20 +4,21 @@ import "./Task.css"
 import Radio from "@material-ui/core/Radio"
 import {FormControlLabel} from "@material-ui/core";
 
-function Task({model, isCompleted = false}) {
+function Task({model, isCompleted = false, onComplete = _ => {}}) {
     const {title} = model
 
     return <FormControlLabel
         className="task"
         control={<Radio />}
         label={title}
+        onClick={onComplete}
         checked={isCompleted} />
 }
 
-export function PendingTask({model}) {
+export function PendingTask({model, onComplete = _ => {}}) {
     return (
         <li>
-            <Task model={model} />
+            <Task model={model} onComplete={onComplete} />
         </li>
     )
 }
