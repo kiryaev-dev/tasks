cee1c672b7d2c93e6e85e147dd1036af2488495e
kiryaev-dev <kiryaev.dev@gmail.com>
2021-04-26 15:34:06 +0500

    Added FAB

--- src/ui/Application.jsx
+++ src/ui/Application.jsx
@@ -1,10 +1,14 @@
 import React from "react"
 import "./Application.css"
+import {Fab} from "@material-ui/core";
+import {AddOutlined} from "@material-ui/icons";
 
 export default function Application() {
     return (
         <main className="application">
-            
+            <Fab variant="extended" className="add-sheet-button" color="secondary" >
+                <AddOutlined className="icon" /> Add sheet
+            </Fab>
         </main>
     )
 }
