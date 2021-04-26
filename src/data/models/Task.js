91285362574e132dcf3afd6af1414c5d5ebcef87
kiryaev-dev <kiryaev.dev@gmail.com>
2021-04-26 18:04:58 +0500

    Added UUID to models

--- src/data/models/Task.js
+++ src/data/models/Task.js
@@ -1,7 +1,9 @@
+import {v4 as generateUUID} from "uuid"
 
 export default class Task {
     constructor(message) {
         this.title = message
         this.created = Date.now()
+        this.uuid = generateUUID()
     }
 }
